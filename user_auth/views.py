56f5f269cbbbfe701ffcb88e5c8f39e69f462e05
Khailas R <khailas303@gmail.com>
2021-10-22 16:44:14 +0530

    token initialized

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, redirect
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
 from django.contrib.auth import login, authenticate
-from .forms import SignUpForm
-from .tokens import account_activaton_token
-from django.utils.encoding import force_text
+from django.utils.encoding import force_text, force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
+from django.db import IntegrityError
+
+from .forms import SignUpForm
+from .tokens import account_activaton_token
 
 
 def signup(request, *args, **kwargs):
@@ -40,11 +43,11 @@ def signup(request, *args, **kwargs):
                         force_bytes(user.pk)
                         ),
                     'token': account_activaton_token.make_token(user),
-            })
+            })  # feeds results into Http response
 
             user.email_user(subject, message)
             return redirect('activation_sent')
-        # this no longer authenticats the user until they confirm 
+        # this no longer authenticats the user until they confirms.
 
             # username = form.cleaned_data.get('username')
             # the_password = form.cleaned_data.get('password1')
@@ -64,6 +67,17 @@ def activate(request, uidb64, token, *args, **kwargs):
     try: 
         uid = force_text(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
-    except:
-        pass
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
         
+    if user is not None and account_activaton_token.check_token(user, token):
+        user.is_active = True
+        user.profile.signup_confirmation = True
+        user.save()
+        
+        login(request, user)
+        return redirect('/')
+    
+    else:   # if activation fails
+        # return render(request, 'user_auth/activation_invalid.html', {})
+        return HttpResponse('Activation link is invalid')
