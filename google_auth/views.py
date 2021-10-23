78839fd83432da5b1ecd4468d3dac2bc230a7d15
Khailas R <khailas303@gmail.com>
2021-10-23 16:06:34 +0530

    updated the bytes conversion area

--- google_auth/views.py
+++ google_auth/views.py
@@ -8,10 +8,17 @@ from django.http import HttpResponseRedirect, HttpResponseBadRequest
 from googleapiclient.discovery import build
 from .models import CredentialsModel
 import httplib2
+from dotenv import load_dotenv
+import os
 
+
+CLIENT_SECRETS = settings.GOOGLE_OAUTH2_CLIENT_SECRETS_JSON
+
+load_dotenv()
+GOOGLE_CONSOLE_KEY = os.getenv('MY_GOOGLE_CONSOLE_KEY')
 
 FLOW = flow_from_clientsecrets(
-    settings.GOOGLE_OAUTH2_CLIENT_SECRETS_JSON,
+    CLIENT_SECRETS,
     scope='https://www.googleapis.com/auth/gmail.readonly',
     redirect_uri='http://127.0.0.1:8000/oauth2callback',
     prompt='consent'
@@ -22,16 +29,18 @@ def gmail_authenticate(request, *args, **kwrgs):
     storage = DjangoORMStorage(CredentialsModel, 'id', request.user, 'credential')
     credential = storage.get()
     
-    if credential is None or credential.invalid:
+    if credential is None or credential.invalid is True:
         FLOW.params['state'] = xsrfutil.generate_token(settings.SECRET_KEY, request.user)
         
         authorize_url = FLOW.step1_get_authorize_url()
+        # f = FLOW(id=request.user, flow=FLOW)
+        # f.save()
         return HttpResponseRedirect(authorize_url)
     
     else:
         http = httplib2.Http()  # handles caching, keep alive, compression, redirects etc.
         http = credential.authorize(http)
-        service = build('gmail', 'v1', http=http)
+        service = build('gmail', 'v1', http=http, developerKey=GOOGLE_CONSOLE_KEY)
         print('access_token = ', credential.access_token)
         status = True
         
@@ -42,11 +51,11 @@ def gmail_authenticate(request, *args, **kwrgs):
     
 # callback url
 def auth_return(request, *args, **kwargs):
-    str_state = request.GET.get('state')
-    get_state = bytes(str_state, 'utf-8')
-
+    # str_state = request.GET.get('state')
+    # get_state = bytes(str_state, encoding='utf-8')
+    is_private = str(request.GET.get['state'])
     if not xsrfutil.validate_token(
-        settings.SECRET_KEY, get_state, request.user
+        settings.SECRET_KEY, is_private, request.user
         ):  # validates token with secret key
         return HttpResponseBadRequest()           # 400 status code. request could not be understood by server cz of malformed syntax
     
