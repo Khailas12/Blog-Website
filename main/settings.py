b4a77cbbb9cbb14910784d4200e78197b83d3e43
Khailas R <khailas303@gmail.com>
2021-10-24 17:13:46 +0530

    auth settings updated

--- main/settings.py
+++ main/settings.py
@@ -41,18 +41,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
     'django.contrib.sites',
 
     'mysite',
     'user_auth',
-    'google_auth'
+    'google_auth',
 
     # # google auth based
-    # 'allauth',
-    # 'allauth.account',
-    # 'allauth.socialaccount',
-    # 'allauth.socialaccount.providers.google',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -146,3 +145,30 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ['static']
 GOOGLE_OAUTH2_CLIENT_SECRETS_JSON = 'client_secrets.json'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
+SITE_ID = 1 
+LOGIN_REDIRECT_URL = '/'    # this redirects url upon succesful login
+
+# additional configs
+SOCIALACCOUT_QUERY_EMAIL = True
+ACCOUNT_LOGOUT_ON_GET = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_EMAIL_REQUIRED = True
+
+# the scope receives user's email address after succesful login
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
