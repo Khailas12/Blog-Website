4d95a9f79a3adc0d12a63afb631fb470349250c3
Khailas R <khailas303@gmail.com>
2021-10-30 19:58:33 +0530

    replaced sqlite3 with postgresql

--- main/settings.py
+++ main/settings.py
@@ -80,10 +80,23 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+postgres_pswd = os.getenv('POSTGRESQL_PSWD')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog3',
+        'USER': 'khailas12',
+        'PASSWORD': postgres_pswd,
+        'HOST': 'localhost',
+        'POST': '',  
     }
 }
 
