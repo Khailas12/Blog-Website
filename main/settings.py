a20baa84811152b540d876a5fa23b9020a77ffaa
Khailas R <khailas303@gmail.com>
2021-10-30 20:53:00 +0530

    retrieved sqlite3

--- main/settings.py
+++ main/settings.py
@@ -80,25 +80,25 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-postgres_pswd = os.getenv('POSTGRESQL_PSWD')
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blog3',
-        'USER': 'khailas12',
-        'PASSWORD': postgres_pswd,
-        'HOST': 'localhost',
-        'POST': '',  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# postgres_pswd = os.getenv('POSTGRESQL_PSWD')
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'blog3',
+#         'USER': 'khailas12',
+#         'PASSWORD': postgres_pswd,
+#         'HOST': 'localhost',
+#         'POST': '',  
+#     }
+# }
 
 
 # Password validation
