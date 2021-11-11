66a0384d8612b4fd5aa9de6a2195eb7ce31c1673
Khailas R <khailas303@gmail.com>
2021-11-11 16:17:18 +0530

    updated

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,6 @@ from django.contrib.auth.views import LogoutView
 from user_auth import views as auth_view
 # from user_auth.views import signup
 from django.conf.urls import url
-from google_auth import views as g_view
 from register.views import register, login, logout_user, activate
 
 
@@ -23,10 +22,6 @@ urlpatterns = [
 
     path('activate/<uidb64>/<token>/', activate, name='activate'),
     # path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', activate,name='activate'),
-
-    url(r'^gmailAuthenticate/', g_view.gmail_authenticate, name='gmailauth'),
-    url(r'^oauth2callback/', g_view.auth_return),
-    url(r'^$ghome/', g_view.user_check),
 
     url(r'^signup/$', auth_view.signup, name='signup'),
     # path('signup/', auth_view.signup, name='signup'),
